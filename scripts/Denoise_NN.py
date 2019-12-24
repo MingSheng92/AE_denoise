19220cabf40db31679994153dcbd4dbecc8fed43
MingSheng92 <58675240+MingSheng92@users.noreply.github.com>
2019-12-24 15:05:19 +1100

    Add files via upload

---
+++ scripts/Denoise_NN.py
@@ -0,0 +1,42 @@
+from keras.models import Sequential, Model
+from keras.layers import Input, Dense, Dropout, Flatten
+from keras.layers import Conv2DTranspose, Conv2D, MaxPooling2D, BatchNormalization, UpSampling2D
+from keras.callbacks import EarlyStopping
+from keras.optimizers import RMSprop, Adam, SGD, Adadelta
+from keras import regularizers
+from keras.callbacks import ReduceLROnPlateau
+import keras.backend as K
+
+# custom loss function to measure image noise ratio 
+def PSNRLoss(y_true, y_pred):
+    """
+    PSNR is Peek Signal to Noise Ratio, which is similar to mean squared error.
+    It can be calculated as
+    PSNR = 20 * log10(MAXp) - 10 * log10(MSE)
+    When providing an unscaled input, MAXp = 255. Therefore 20 * log10(255)== 48.1308036087.
+    However, since we are scaling our input, MAXp = 1. Therefore 20 * log10(1) = 0.
+    Thus we remove that component completely and only compute the remaining MSE component.
+    """
+    #tf.math.log
+    return -10. * K.log(K.mean(K.square(y_pred - y_true))) / K.log(10.)
+
+def createModel(img_size):
+    input_layer = Input(shape=(img_size[0], img_size[1], 1))
+
+    x = Conv2D(64, (5, 5), activation='relu', padding='same')(input_layer)
+    x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+    x = BatchNormalization(input_shape=(img_size[0], img_size[1], 1))(x)
+
+    x = Dropout(0.8, name='Dropout')(x)
+
+    x = Conv2DTranspose(32, (3, 3), activation='relu', padding='same')(x)
+    x = Conv2DTranspose(64, (5, 5), activation='relu', padding='same')(x)
+    x = BatchNormalization(input_shape=(img_size[0], img_size[1], 1))(x)
+
+    output = Conv2D(1, (1, 1), padding='same')(x)
+
+    NN_model = Model(input_layer, output)
+    adam = Adam(lr=1e-3)
+    NN_model.compile(optimizer=adam, loss='mse', metrics=[PSNRLoss])
+    
+    return NN_model
